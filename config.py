b90bcea0c67a8ef30beef5214b0f6c681172cdac
riyakannan312 <riyakannan312@gmail.com>
2025-01-13 01:32:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,6 +22,8 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 BOT_USERNAME = environ.get("BOT_USERNAME", "desileaksfilesbot") # without @
 PORT = environ.get("PORT", "8080")
 
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002014891006').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
+
 # Clone Info :-
 CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 
@@ -42,8 +44,6 @@ AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Second
 
 # Channel Information
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002090853196"))
-
-AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002014891006').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
 
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
