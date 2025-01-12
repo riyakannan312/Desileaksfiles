4269ede2ecc97a94ec8cd7a32ccd91cd8abcdd6c
riyakannan312 <riyakannan312@gmail.com>
2025-01-13 01:26:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -66,8 +66,7 @@ async def start(client, message):
                 await message.reply_text(text=f"<b>👋 Hello {message.from_user.mention},\n\nPlease join the channel then click on try again button. 😇</b>", reply_markup=InlineKeyboardMarkup(btn))
                 return
         except Exception as e:
-            print(e)
-    username = (await client.get_me()).username   
+            print(e)   
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT.format(message.from_user.id, message.from_user.mention))
