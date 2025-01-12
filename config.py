9fe0f4ed3612377c5fc91bcea917b42773c161e5
riyakannan312 <riyakannan312@gmail.com>
2025-01-13 00:23:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ API_HASH = environ.get("API_HASH", "f2205657fd3b0b40e9a0b11aac366656")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7891431471:AAGpcqVR9KWFeUWWVMAhj4jmGmkmFB-Dtbg")
 
 PICS = (environ.get('PICS', 'https://desileaks.in/wp-content/uploads/2024/08/5d0ac3e9-73d0-485f-901a-871bf6ea09cb.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1871899094', '6866849202').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1871899094 6866849202 1032182348').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "desileaksfilesbot") # without @
 PORT = environ.get("PORT", "8080")
 
